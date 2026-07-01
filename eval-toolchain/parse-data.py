9c3360bec590674a2d21c156239abb77d8c6b5e9
Rai Trouvain <raitrouvain@gmail.com>
2026-07-01 13:46:37 +0200

    more eval

--- eval-toolchain/parse-data.py
+++ eval-toolchain/parse-data.py
@@ -1,4 +1,5 @@
 import csv
+from datetime import datetime
 import frontmatter
 import glob
 from pprint import pprint
@@ -44,16 +45,54 @@ with open("eval-data.csv", "w", newline="") as f:
 # ======================================== Time-Series Data ==========================================
 
 DATA_KEYS_2 = [
+    'p_id',
+    'group',
+    'task',
+    'map',
     'timestamp',
     'tag',
-    'note'
+    'note',
+    'timedelta'
 ]
+
+MAPS = {
+    'AM': {
+        'solvability_pixie': 'LA_4',
+        'solvability_miro': 'LA_3',
+        'pacing_pixie': 'LA_2',
+        'pacing_miro': 'LA_1'
+    },
+    'AP': {
+        'solvability_pixie': 'LA_3',
+        'solvability_miro': 'LA_4',
+        'pacing_pixie': 'LA_1',
+        'pacing_miro': 'LA_2'
+    },
+    'BM': {
+        'solvability_pixie': 'LA_3',
+        'solvability_miro': 'LA_4',
+        'pacing_pixie': 'LA_1',
+        'pacing_miro': 'LA_2'
+    },
+    'BP': {
+        'solvability_pixie': 'LA_4',
+        'solvability_miro': 'LA_3',
+        'pacing_pixie': 'LA_2',
+        'pacing_miro': 'LA_1'
+    }
+}
+
+def get_map(heading: str, group: str) -> str:
+    return MAPS[group][heading]
+
+time_fmt = '%H:%M:%S'
 
 time_series_data = []
 count = 0
 for path in files:
-    if count > 0:
-        break
+    print(f'parsing {path}')
+    #if count > 0:
+    #    break
     with open(path) as f:
         metadata, content = frontmatter.parse(f.read())
 
@@ -70,33 +109,53 @@ for path in files:
 
         timestamp_re = r'^(0[0-1]|1[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]'
         for line in lines:
+            print(line)
             if line.startswith('####'):
                 current_heading = line.removeprefix('#### ')
-                #print(current_heading)
+                print(current_heading)
 
                 if current_heading == 'General Feedback':
                     break
 
             elif re.match(timestamp_re, line):
+                print('match found')
                 timestamp = line[0:8]
                 tag = line[9:12]
                 note = line[13:]
 
                 file_data[current_heading].append({
+                    'p_id' : metadata['p_id'],
+                    'group' : metadata['group'],
+                    'task' : current_heading,
+                    'map' : get_map(current_heading, metadata['group']),
                     'timestamp' : timestamp,
                     'tag' : tag,
-                    'note' : note,
+                    'note' : note, 
                 })
 
         for mode, series in file_data.items():
+            if (len(series) == 0):
+                pprint(series)
+                continue
+            start_time = datetime.strptime(series[0]['timestamp'], time_fmt)
+
             with open(f'time_series/{metadata['p_id']}/{mode}.csv', "w", newline="") as f:
                 w = csv.DictWriter(f, DATA_KEYS_2)
                 w.writeheader()
                 for d in series:
+                    d['timedelta'] = (datetime.strptime(d['timestamp'], time_fmt) - start_time).seconds
                     w.writerow(d)
+
+            time_series_data += series
 
         pprint(file_data)
 
-        # time_series_data.append(ast_dict)
-    count += 1
+with open(f'time_series/all.csv', "w", newline="") as f:
+    w = csv.DictWriter(f, DATA_KEYS_2)
+    w.writeheader()
+    for d in time_series_data:
+        w.writerow(d)
+
+
+    # count += 1
 
