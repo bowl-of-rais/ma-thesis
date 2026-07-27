19049b41fde393e520e143260a194ef2c52414c0
Rai Trouvain <raitrouvain@gmail.com>
2026-07-28 00:07:13 +0200

    eval update 2026-07-28

--- ma-eval/parse-data.py
+++ ma-eval/parse-data.py
@@ -39,12 +39,66 @@ DATA_KEYS = [
     's_sob_pixie'
 ]
 
-with open("eval-data.csv", "w", newline="") as f:
+with open("protocol-data.csv", "w", newline="") as f:
     w = csv.DictWriter(f, DATA_KEYS)
     w.writeheader()
     for d in data:
         w.writerow(d)
 
+QUESTIONNAIRE_DATA_KEYS = [
+    "Timestamp",
+    "Participant ID",
+    "Age",
+    "Gender",
+    "Education (study course and whether its ongoing)",
+    "Education Codified",
+    "Occupation:",
+    "Number of projects/games you worked on (rough estimate)",
+    "Projects Codified",
+    "Experience with digital whiteboards (e.g. Miro, Figjam)",
+    "Whiteboard Experience Codified",
+    "Experience with game design tools (e.g. Arcweave)",
+    "Game Design Tool Experience Codified",
+    "Relevant Background (anything not covered by the other questions):",
+    "SUS-1",
+    "SUS-2",
+    "SUS-3",
+    "SUS-4",
+    "SUS-5",
+    "SUS-6",
+    "SUS-7",
+    "SUS-8",
+    "SUS-9",
+    "SUS-10",
+    "FQ1",
+    "UEQS-1",
+    "UEQS-2",
+    "UEQS-3",
+    "UEQS-4",
+    "UEQS-5",
+    "UEQS-6",
+    "UEQS-7",
+    "UEQS-8",
+    "C-Q1",
+    "C-Q2",
+    "C-Q3",
+    "C-Q4",
+    "FQ2"
+]
+
+combined_data = []
+with open('questionnaire-responses-preprocessed.csv', newline='') as questionnaire_data:
+    reader = csv.DictReader(questionnaire_data)
+    for row in reader:
+        matching_protocol = next(item for item in data if item["p_id"] == row["Participant ID"])
+        merged_row = row | matching_protocol
+        combined_data.append(merged_row)
+
+with open("combined-data.csv", "w", newline="") as f:
+    w = csv.DictWriter(f, DATA_KEYS + QUESTIONNAIRE_DATA_KEYS)
+    w.writeheader()
+    for d in combined_data:
+        w.writerow(d)
 
 # ======================================== Time-Series Data ==========================================
 
@@ -92,6 +146,7 @@ def get_map(heading: str, group: str) -> str:
 time_fmt = '%H:%M:%S'
 
 time_series_data = []
+feedback_data = []
 count = 0
 for path in files:
     print(f'parsing {path}')
@@ -106,7 +161,7 @@ for path in files:
             'solvability_pixie': [],
             'solvability_miro': [],
             'pacing_pixie': [],
-            'pacing_miro': []
+            'pacing_miro': [],
         }
         
         current_heading = ''
@@ -118,10 +173,7 @@ for path in files:
                 current_heading = line.removeprefix('#### ')
                 print(current_heading)
 
-                if current_heading == 'General Feedback':
-                    break
-
-            elif re.match(timestamp_re, line):
+            elif current_heading != 'feedback_comments_observations' and re.match(timestamp_re, line):
                 print('match found')
                 timestamp = line[0:8]
                 tag = line[9:12]
@@ -133,6 +185,17 @@ for path in files:
                     'task' : current_heading,
                     'map' : get_map(current_heading, metadata['group']),
                     'timestamp' : timestamp,
+                    'tag' : tag,
+                    'note' : note, 
+                })
+
+            elif current_heading == 'feedback_comments_observations' and line.strip():
+                tag = line[0:3]
+                note = line[4:]
+
+                feedback_data.append({
+                    'p_id' : metadata['p_id'],
+                    'group' : metadata['group'],
                     'tag' : tag,
                     'note' : note, 
                 })
@@ -160,6 +223,18 @@ with open(f'time_series/all.csv', "w", newline="") as f:
     for d in time_series_data:
         w.writerow(d)
 
+DATA_KEYS_FEEDBACK = [
+    'p_id',
+    'group',
+    'tag',
+    'note'
+]
+
+with open(f'feedback_comments_observations.csv', "w", newline="") as f:
+    w = csv.DictWriter(f, DATA_KEYS_FEEDBACK)
+    w.writeheader()
+    for d in feedback_data:
+        w.writerow(d)
 
     # count += 1
 
